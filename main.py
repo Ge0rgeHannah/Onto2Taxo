ff598f748b2f7c8eafb5618356a777ac7f56446b
George Hannah <georgehannah@hotmail.co.uk>
2024-08-22 11:05:05 +0100

    Added class and relation isolating SPARQL queries

--- main.py
+++ main.py
@@ -3,9 +3,46 @@ import json
 import rdflib
 
 
-# Query the ontology and return the results as a list
-def sparql_query(onto, query):
-    pass
+# Query the ontology and return the classes as a list
+def classes_query(onto):
+
+    # This query will need to be changed slightly from different ontologies        BIND(STRAFTER(STR(?class), "#") AS ?className)
+    query_1 = '''
+    SELECT ?className WHERE {
+        ?class rdf:type owl:Class .
+        BIND(STRAFTER(STR(?class), "#") AS ?className)
+    }
+    '''
+
+    results = []
+    raw_result = onto.query(query_1)
+    for row in raw_result:
+        results.append(str(row.className))
+
+    return results
+
+
+# Query the ontology and return the subClassOf relations as a list
+def subClasses_query(onto):
+    
+    # This query will need to be changed slightly from different ontologies        BIND(STRAFTER(STR(?class), "#") AS ?className)
+    query_2 = '''
+    SELECT ?parent ?child WHERE {
+        ?p rdf:type owl:Class .
+        ?c rdf:type owl:Class .
+        ?c rdfs:subClassOf ?p .
+        BIND(STRAFTER(STR(?p), "#") AS ?parent)
+        BIND(STRAFTER(STR(?c), "#") AS ?child)
+    }
+    '''
+
+    results = []
+    raw_result = onto.query(query_2)
+    for row in raw_result:
+        relation = [str(row.parent), str(row.child)]
+        results.append(relation)
+
+    return results
 
 
 # Create a list of each node with its associated id
@@ -24,9 +61,14 @@ def run(args):
     onto = args.onto
     taxo = args.taxo
 
+    g = rdflib.Graph()
+    g.parse(onto)
+
     # TODO: Query ontology for all classes
+    all_classes = classes_query(g)
 
     # TODO: Query ontology for all rdfs:subClassOf relationships
+    all_relations = subClasses_query(g)
 
     # TODO: Populate nodes list
 
